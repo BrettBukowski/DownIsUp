251aa79afbf62a155b5473b8a906f2617e45100f
BrettBukowski <brett.bukowski@gmail.com>
2011-03-16 20:48:44 -0600

    Boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Downisup::Application.config.secret_token = 'ad59d7ca7c638edd01d7845d2561051f52e2043e313895a6b7fc004db08ddc403ecf883c6aa0a9ae0443d2464b3cc1c7c334739a73918890770bf85f6bb784ff'
