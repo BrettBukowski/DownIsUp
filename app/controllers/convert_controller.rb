94d5e07fd5153a986b85d701b6e6485627f051c4
BrettBukowski <brett.bukowski@gmail.com>
2011-03-21 21:49:29 -0600

    Barebones but works

--- app/controllers/convert_controller.rb
+++ app/controllers/convert_controller.rb
@@ -2,8 +2,12 @@ require 'kramdown'
 
 class ConvertController < ApplicationController
   def all
-    if (parms[:text] && request.xhr?)
-      render :text => Kramdown::Document.new(text).to_html
+    if (params[:text] && request.xhr?)
+      begin
+        render :text => Kramdown::Document.new(params[:text]).to_html
+      rescue Exception => e
+        render :text => ""
+      end
     end
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,3 @@
-ENV['GEM_PATH'] = '/Library/Ruby/Gems/1.8:/Users/brettbukowski/.gem/ruby/1.8:/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/gems/1.8'
 
 # Load the rails application
 require File.expand_path('../application', __FILE__)
